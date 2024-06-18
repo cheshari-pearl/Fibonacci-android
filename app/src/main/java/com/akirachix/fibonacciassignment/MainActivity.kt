03a648ef8d56e10c1adbe7ebd61013d38ff13756
cheshari-pearl <chesharipearl@gmail.com>
2024-06-18 15:56:24 +0300

    First commit

--- app/src/main/java/com/akirachix/fibonacciassignment/MainActivity.kt
+++ app/src/main/java/com/akirachix/fibonacciassignment/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.akirachix.fibonacciassignment
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.akirachix.fibonacciassignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val numbers = fibonacci(100)
+        binding.rvNumbers.layoutManager = LinearLayoutManager(this)
+
+        val numberAdapter = FibonacciRecyclerViewAdapter(numbers)
+        binding.rvNumbers.adapter = numberAdapter
+
+
+    }
+
+    fun fibonacci(n: Int): List<Int> {
+        val numbers = mutableListOf(0, 1)
+        while (numbers.size < n) {
+            numbers.add(numbers[numbers.lastIndex] + numbers[numbers.lastIndex - 1])
         }
+        return numbers
     }
 }
